22f7baee0c8312b4e9ac7bbf64ae944266b7db2d
KhatiwadaPrajwal056 <khatiwadaprajwal22@icloud.com>
2023-05-06 11:54:12 +0545

    practice

--- Recent Practice/chapter6.py
+++ Recent Practice/chapter6.py
@@ -1,10 +1,16 @@
 # Conditional expressions
+#truthy and falsey
 
 
 
 # In and IS
 # a is none
 # In is used in for loop
-name=input("ENter name ")
-name= name.lower()
-print(name)
+# name=input("ENter name ")
+# name= name.lower()
+# print(name)
+
+pk=True
+sk=False
+if pk is True and sk is True:
+    print("dfdfdd")
