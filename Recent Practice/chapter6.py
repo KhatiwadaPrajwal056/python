015fa2efaeeb0c34f2db195aef6fdb185ce9b4e2
KhatiwadaPrajwal056 <khatiwadaprajwal22@icloud.com>
2023-05-07 14:04:00 +0545

    practice

--- Recent Practice/chapter6.py
+++ Recent Practice/chapter6.py
@@ -10,7 +10,15 @@
 # name= name.lower()
 # print(name)
 
-pk=True
-sk=False
-if pk is True and sk is True:
-    print("dfdfdd")
+# pk=True
+# sk=False
+# if pk is True and sk is True:
+#     print("dfdfdd")
+
+# difference in  IS and ==
+
+print(True is 1)
+print('1' is 1) #needs to be same string
+print([] is 1) #different string points a diffreent memory location , so cant be true
+print([1,2] is [1,2]) #false, coz they are different in memory locations
+
--- Recent Practice/chapter7.py
+++ Recent Practice/chapter7.py
@@ -141,7 +141,7 @@ for i in range(1,6):
     for k in range(1,i+1):
         print(end="* ")  
     print("\n")
-'''
+
 
 6.
 
@@ -150,6 +150,28 @@ for i in range(1,6):
     for j in range(1,i+1):
         print(k+1,end=" ")
         k+=1
-    print("\n")
+    print("\n") 
+
+for i ,char in enumerate(range(100)):
+    if char==50:
+        print(f'The index of {char} is {i}') 
+
+
+#Picture or pattern printing
+picture=[
+    [0,1,1,0,0,1,0,1],
+    [1,0,0,1,0,1,0,1],
+    [0,0,1,0,0,1,0,0],
+    [1,0,0,1,0,1,0,1],
+    [1,0,0,1,1,1,0,1]
+]
+for i in picture:
+    for pixles in i:
+        if pixles==0:
+            print(' ',end='')
+        else:
+            print('*',end='')
+    print() '''
+    
 
 
